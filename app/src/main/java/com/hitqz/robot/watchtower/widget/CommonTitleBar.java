af288dfcadd9b14fdf80a1474d5b2bf4deae8320
iponkan <iponkan@gmail.com>
2019-07-09 17:05:54 +0800

    commontilebar

--- app/src/main/java/com/hitqz/robot/watchtower/widget/CommonTitleBar.java
+++ app/src/main/java/com/hitqz/robot/watchtower/widget/CommonTitleBar.java
@@ -1,13 +1,25 @@
 package com.hitqz.robot.watchtower.widget;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class CommonTitleBar extends FrameLayout {
+import com.hitqz.robot.watchtower.R;
+
+public class CommonTitleBar extends FrameLayout implements View.OnClickListener {
+
+    private ViewGroup vpBack;
+    private Context context;
+    private TextView tvTitle;
 
     public CommonTitleBar(@NonNull Context context) {
         this(context, null);
@@ -19,5 +31,28 @@ public class CommonTitleBar extends FrameLayout {
 
     public CommonTitleBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.context = context;
+        setBackground(new ColorDrawable(getResources().getColor(R.color.colorPrimary)));
+        LayoutInflater.from(context).inflate(R.layout.layout_title_bar, this, true);
+        vpBack = findViewById(R.id.fl_back);
+        vpBack.setOnClickListener(this);
+        tvTitle = findViewById(R.id.tv_title_bar_title);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (vpBack == v) {
+            if (context instanceof Activity) {
+                ((Activity) context).finish();
+            }
+        }
+    }
+
+    public void setTitle(String title) {
+        tvTitle.setText(title);
     }
 }
